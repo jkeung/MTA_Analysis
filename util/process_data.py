b6de19fae26fd376de1587571a982661cc6c73ea
Andre Gatorano <agatorano@gmail.com>
2015-09-22 17:32:33 -0400

    changes to process_data.py

--- util/process_data.py
+++ util/process_data.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from datetime import datetime,timedelta
 
 import urllib2
@@ -6,19 +7,29 @@ import pandas as pd
 
 
 def get_data():
-    endDate=datetime.strptime('150919','%y%m%d')
-    currentDate=datetime.strptime('141019','%y%m%d')
-    baseLink='http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
 
+    time1 = time.time()
+    endDate=datetime.strptime('150919','%y%m%d')
+    currentDate=datetime.strptime('141025','%y%m%d')
+    baseLink='http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
     df=pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt')
     while(currentDate<endDate):
         currentDate=currentDate+timedelta(days=7)
         print currentDate
         df.append(pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt'))
+    time2 = time.time()
+    print 'function took %0.3f ms' % ((time2-time1)*1000.0)
+
+    df.to_csv("MTA_DATA.csv")
 
     return df
 
-def creat_dict_by_STATION(data):
+def get_data_local():
+
+    data = pd.read_csv("MTA_DATA.csv")
+    return data
+
+def create_dict_by_STATION(data):
     UniqueNames = data["STATION"].unique()
     DataFrameDict = {elem : pd.DataFrame for elem in UniqueNames}
     for key in DataFrameDict.keys():
@@ -26,7 +37,7 @@ def creat_dict_by_STATION(data):
 
     return DataFrameDict
 
-def addDayOfWeek(DataFrameDict):
+def add_Day_Month(DataFrameDict):
     """
     Given a dict where keys are the stations and values are panda timeseries
     It will add a column of day of week 0 through 6 with 0 being Sunday
@@ -39,8 +50,15 @@ def addDayOfWeek(DataFrameDict):
 
     return DataFrameDict
 
-def main():
+def obtain_full_data():
     data = get_data()
+    dicts = create_dict_by_STATION(data)
+    day_month_dic = add_Day_Month(dicts)
+
+    return day_month_dic
+
+def main():
+    data = get_data_local()
     dicts = create_dict_by_STATION(data)
     day_month_dic = add_Day_Month(dicts)
 
