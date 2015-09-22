c57c7fe80ac5794960b1b52b6b39b805f3c5682f
Andre Gatorano <agatorano@gmail.com>
2015-09-22 18:55:31 -0400

    Tons of changes in process.py

--- util/process_data.py
+++ util/process_data.py
@@ -2,23 +2,20 @@ import sys
 import time
 from datetime import datetime,timedelta
 
+from pprint import pprint
 import urllib2
 import pandas as pd
 
 
 def get_data():
 
-    time1 = time.time()
     endDate=datetime.strptime('150919','%y%m%d')
     currentDate=datetime.strptime('141025','%y%m%d')
     baseLink='http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
     df=pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt')
     while(currentDate<endDate):
         currentDate=currentDate+timedelta(days=7)
-        print currentDate
         df.append(pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt'))
-    time2 = time.time()
-    print 'function took %0.3f ms' % ((time2-time1)*1000.0)
 
     df.to_csv("MTA_DATA.csv")
 
@@ -30,37 +27,53 @@ def get_data_local():
     return data
 
 def create_dict_by_STATION(data):
+    
     UniqueNames = data["STATION"].unique()
     DataFrameDict = {elem : pd.DataFrame for elem in UniqueNames}
     for key in DataFrameDict.keys():
-        DataFrameDict[key] = data[:][data["C/A"] == key]
+             DataFrameDict[key] = data[:][data["STATION"] == key]
 
     return DataFrameDict
 
-def add_Day_Month(DataFrameDict):
+def dict_station_time_totals(DataFrameDict):
+    
+    sum_dict = {key: (DataFrameDict[key].groupby(['DATE','TIME']).aggregate(sum)-DataFrameDict[key].groupby(['DATE','TIME']).aggregate(sum).shift(1)) for key in DataFrameDict}
+
+    return sum_dict
+
+
+def add_Day_Month(data):
     """
     Given a dict where keys are the stations and values are panda timeseries
     It will add a column of day of week 0 through 6 with 0 being Sunday
     """
-    for key in DataFrameDict:
-        DataFrameDict[key]['DAY']=DataFrameDict[key]['DATE'].apply(lambda x: datetime.strptime(x,'%m/%d/%Y').strftime('%w'))
+    data['DAY'] = data['DATE'].apply(lambda x: datetime.strptime(x,'%m/%d/%Y').strftime('%w'))
+    data['MONTH'] = data['DATE'].apply(lambda x: datetime.strptime(x,'%m/%d/%Y').strftime('%m'))
 
-    for key in DataFrameDict:
-        DataFrameDict[key]['MONTH']=DataFrameDict[key]['DATE'].apply(lambda x: datetime.strptime(x,'%m/%d/%Y').strftime('%m'))
-
-    return DataFrameDict
+    return data
 
 def obtain_full_data():
-    data = get_data()
+    data = get_data_local()
+    data = add_Day_Month(data)
     dicts = create_dict_by_STATION(data)
-    day_month_dic = add_Day_Month(dicts)
 
     return day_month_dic
 
+def get_month_sum(DataFrameDict):
+
+    month_dict = {}
+    for key in DataFrameDict:
+        month_dict[key] = DataFrameDict[key].groupby(['STATION', 'MONTH']).aggregate(sum)
+
+    pprint(month_dict)
+
 def main():
     data = get_data_local()
+    data = add_Day_Month(data)
     dicts = create_dict_by_STATION(data)
-    day_month_dic = add_Day_Month(dicts)
+    get_month_sum(dicts)
+    #time_dict = dict_station_time_totals(dicts)
+    #print(time_dict)
 
 if __name__ == '__main__':
     main()
