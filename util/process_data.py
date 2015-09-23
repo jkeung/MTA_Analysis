b8e2c07816c8df78770e0f157e3c27cf5211a3a8
Andre Gatorano <agatorano@gmail.com>
2015-09-23 10:49:06 -0400

    process_data.py is now PEP8 kosher

--- util/process_data.py
+++ util/process_data.py
@@ -1,29 +1,28 @@
-import sys
-import time
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 from pprint import pprint
-import urllib2
 import pandas as pd
 
 
 def get_data():
     """
     Downloads data from online MTA data source
-    It loops through all recent data, and appends it to a single csv datafile 
+    It loops through all recent data, and appends it to a single csv datafile
     """
 
-    endDate=datetime.strptime('150919','%y%m%d')
-    currentDate=datetime.strptime('141025','%y%m%d')
-    baseLink='http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
-    df=pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt')
-    while(currentDate<endDate):
-        currentDate=currentDate+timedelta(days=7)
+    endDate = datetime.strptime('150919', '%y%m%d')
+    currentDate = datetime.strptime('141025', '%y%m%d')
+    baseLink = 'http://web.mta.info/developers/data/nyct/turnstile/turnstile_'
+    df = pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt')
+    while(currentDate < endDate):
+        currentDate = currentDate+timedelta(days=7)
         df.append(pd.read_csv(baseLink+currentDate.strftime('%y%m%d')+'.txt'))
 
     return df
 
-def save_file(name,data):
+
+def save_file(name, data):
+
     """
     Saves pandas data to a csv file
     """
@@ -39,29 +38,36 @@ def get_data_local(name):
     data = pd.read_csv(name)
     return data
 
+
 def create_dict_by_STATION(data):
 
     """
     This converts the MTA data frame into a dictionary
     The stations are keys
-    The values are data frames that are the data that has the key in the stations column
+    The values are data frames
+    The data frames are the data that has the key in the stations column
     """
-    
+
     UniqueNames = data["STATION"].unique()
-    DataFrameDict = {elem : pd.DataFrame for elem in UniqueNames}
+    DataFrameDict = {elem: pd.DataFrame for elem in UniqueNames}
     for key in DataFrameDict.keys():
-             DataFrameDict[key] = data[:][data["STATION"] == key]
+        DataFrameDict[key] = data[:][data["STATION"] == key]
 
     return DataFrameDict
+
 
 def dict_station_time_totals(DataFrameDict):
 
     """
     Takes a dictionary of dataframes
-    Returns a data frame containing the difference in entries per station from one time frame to the next
+    Returns a data frame containing
+    the difference in entries per station from one time frame to the next
     """
-    
-    sum_dict = {key: (DataFrameDict[key].groupby(['DATE','TIME']).aggregate(sum)-DataFrameDict[key].groupby(['DATE','TIME']).aggregate(sum).shift(1)) for key in DataFrameDict}
+
+    sum_dict = {key: (DataFrameDict[key].groupby(
+        ['DATE', 'TIME']).aggregate(sum) -
+        DataFrameDict[key].groupby(['DATE', 'TIME']).aggregate(sum).shift(1))
+        for key in DataFrameDict}
 
     return sum_dict
 
@@ -73,10 +79,13 @@ def add_Day_Month(data):
     It will add a column of day of week 0 through 6 with 0 being Sunday
     """
 
-    data['DAY'] = data['DATE'].apply(lambda x: datetime.strptime(x,'%m/%d/%Y').strftime('%w'))
-    data['MONTH'] = data['DATE'].apply(lambda x: datetime.strptime(x,'%m/%d/%Y').strftime('%m'))
+    data['DAY'] = data['DATE'].apply(
+        lambda x: datetime.strptime(x, '%m/%d/%Y').strftime('%w'))
+    data['MONTH'] = data['DATE'].apply(
+        lambda x: datetime.strptime(x, '%m/%d/%Y').strftime('%m'))
 
     return data
+
 
 def get_month_sum(DataFrameDict):
     """"
@@ -87,9 +96,11 @@ def get_month_sum(DataFrameDict):
 
     month_dict = {}
     for key in DataFrameDict:
-        month_dict[key] = DataFrameDict[key].groupby(['STATION', 'MONTH']).aggregate(sum)
+        month_dict[key] = DataFrameDict[key].groupby(
+            ['STATION', 'MONTH']).aggregate(sum)
 
     pprint(month_dict)
+
 
 def obtain_full_data():
     data = get_data_local()
@@ -101,12 +112,11 @@ def obtain_full_data():
 
 def main():
     data = get_data_local("MTA_DATA.csv")
-    #save_file("MTA_DATA.csv",data)
+    save_file("MTA_DATA.csv", data)
     data = add_Day_Month(data)
     dicts = create_dict_by_STATION(data)
     get_month_sum(dicts)
-    #time_dict = dict_station_time_totals(dicts)
-    #print(time_dict)
+    dict_station_time_totals(dicts)
 
 if __name__ == '__main__':
     main()
